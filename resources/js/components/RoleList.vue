603437ab29b9dc944c48f2625b077feab00c35c7
Colin McFadden <cmcfadden@gmail.com>
2020-10-14 20:42:56 -0500

    add filtering by folder to roles

--- resources/js/components/RoleList.vue
+++ resources/js/components/RoleList.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
        
-        <table class="table">
+        <table class="table" v-for="officialGroupType in officialGroupTypes" :key="officialGroupType">
             <thead>
                 <tr>
-                    <th scope="col">Official Departmental Roles</th>
+                    <th scope="col">Official {{ officialGroupType }} Roles</th>
               </tr>
           </thead>
           <tbody v-if="roleList">
-            <tr v-for="(role, key) in roleList.filter(r=>r.official_group_type)"  :key="key">
+            <tr v-for="(role, key) in roleList.filter(r=>r.official_group_type && r.official_group_type.map(gt=>gt.label).includes(officialGroupType))"  :key="key">
                 <td><router-link :to="{ name: 'role', params: { roleId: role.id } }">{{ role.label }}</router-link></td>
                 
             </tr>
@@ -39,6 +39,13 @@
         },
         mounted() {
             this.loadRoles();
+        },
+        computed: {
+            officialGroupTypes: function() {
+                console.log(this.roleList.map(r => r.official_group_type).flat());
+                let allLabels = this.roleList.map(r => r.official_group_type).flat().filter(r=> r).map(r=>r?r.label:false);
+                return [ ... new Set(allLabels)];
+            }
         },
         methods: {
             loadRoles() {
