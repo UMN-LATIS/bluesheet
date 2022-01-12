6f1627797fb7886dafb6ad4d4c433e355a4c2639
Colin McFadden <cmcfadden@gmail.com>
2022-01-11 20:53:16 -0600

    fix mobile

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<title>Groups</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="UTF-8">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 		{{-- <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet"> --}}
 		{{-- <link href="https://fonts.googleapis.com/css?family=Itim" rel="stylesheet"> --}}
