3fc0cc31ad9e92ec7bbe7a29528a4382d74ac645
Colin McFadden <cmcfadden@gmail.com>
2022-03-14 16:55:22 -0500

    some tests!

---
+++ database/seeders/TestDatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+
+class TestDatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $this->call(TestGroupsUsersTableSeeder::class);
+        $this->call(TestGroupsModelHasRolesTableSeeder::class);
+        $this->call(TestGroupsGroupTypesTableSeeder::class);
+        $this->call(TestGroupsOfficialRoleCategoriesTableSeeder::class);
+        $this->call(TestGroupsRolesTableSeeder::class);
+        $this->call(TestGroupsGroupTypeRoleTableSeeder::class);
+        $this->call(ParentOrganizationsTableSeeder::class);
+    }
+}
