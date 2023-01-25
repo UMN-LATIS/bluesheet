fa74f19ffe425ada87ef12dc5dfcfe4098359d37
Colin McFadden <cmcfadden@gmail.com>
2023-01-25 17:27:27 -0600

    add notes

--- resources/js/components/userWithLink.vue
+++ resources/js/components/userWithLink.vue
@@ -1,7 +1,8 @@
 <template>
     <span>
-        <p v-for="member in memberList">
+        <p v-for="member in memberList" >
             <router-link :to="{ name: 'user', params: { userId: member.user.id } }">{{ member.user.displayName }}</router-link>
+            <small class="text-muted d-block">{{ member.notes }}</small>
         </p>
     </span>
 </template>
