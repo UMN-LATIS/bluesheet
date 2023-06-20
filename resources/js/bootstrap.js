016752e3498cc962abaa4ea2ba83b5f3af54c0b4
James Johnson <johnsojr@umn.edu>
2023-06-20 17:26:06 -0500

    change import for popper, jquery, and axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,27 +1,19 @@
-
-// window._ = require('lodash');
-// 
 import sortBy from 'lodash/sortBy';
 import get from 'lodash/get';
 import uniq from 'lodash/uniq';
 import debounce from 'lodash/debounce';
+import Popper from 'popper.js';
+import jquery from 'jquery';
+import axios from 'axios';
+
+import 'bootstrap';
 
 window._ = {};
 window._.get = get;
 window._.debounce = debounce;
+window.Popper = Popper;
+window.$ = window.jQuery = jquery;
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -29,8 +21,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
-
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
@@ -46,20 +37,3 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
