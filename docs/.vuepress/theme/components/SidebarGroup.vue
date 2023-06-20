f0e761dd8f31fe3bd54b42594e6dc7265fbe532b
James Johnson <johnsojr@umn.edu>
2023-06-20 17:20:44 -0500

    use dyanmic import for sidebarlinks require

--- docs/.vuepress/theme/components/SidebarGroup.vue
+++ docs/.vuepress/theme/components/SidebarGroup.vue
@@ -60,10 +60,10 @@ import DropdownTransition from '@theme/components/DropdownTransition.vue'
 export default {
   name: 'SidebarGroup',
   props: ['item', 'open', 'collapsable', 'depth'],
-  components: { DropdownTransition },
-  // ref: https://vuejs.org/v2/guide/components-edge-cases.html#Circular-References-Between-Components
-  beforeCreate () {
-    this.$options.components.SidebarLinks = require('./SidebarLinks.vue').default
+  components: {
+    DropdownTransition,
+    // ref: https://vuejs.org/v2/guide/components-edge-cases.html#Circular-References-Between-Components
+    SidebarLinks: () => import('./SidebarLinks.vue'),
   },
   methods: { isActive }
 }
