e3f42065acc215e9dfec80b01756757b847b4a91
James Johnson <johnsojr@umn.edu>
2023-06-20 17:16:48 -0500

    change requires to import and reorg app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,169 +1,161 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import "./bootstrap";
+import Vue from "vue";
+import Vuex from "vuex";
+import VueRouter from "vue-router";
+import VueSelect from "vue-select";
+import VueTour from "vue-tour";
+import Permissions from "./mixins/permissions";
+import VueMoment from "vue-moment";
+import VTooltip from "v-tooltip";
+import Treeselect from "@riophae/vue-treeselect";
+import Autocomplete from "vuejs-auto-complete";
+import JsonCSV from "vue-json-csv";
+import UserHome from "./components/UserHome.vue";
+import LandingPage from "./components/LandingPage.vue";
+import UserList from "./components/UserList.vue";
+import RoleList from "./components/RoleList.vue";
+import Group from "./components/Group.vue";
+import Role from "./components/Role.vue";
+import GroupList from "./components/GroupList.vue";
+import ReportList from "./components/ReportList.vue";
+import MissingOfficial from "./components/MissingOfficial.vue";
+import LastModified from "./components/LastModified.vue";
+import CeddLike from "./components/ceddLike.vue";
+import orgp from "./components/orgp.vue";
+import Home from "./components/Home.vue";
+import ViewUser from "./components/ViewUser.vue";
+import ViewGroup from "./components/ViewGroup.vue";
+import EditGroup from "./components/EditGroup.vue";
+import Members from "./components/Members.vue";
+import Roles from "./components/Roles.vue";
+import Modal from "./components/Modal.vue";
+import UserLookup from "./components/UserLookup.vue";
+import CreateGroup from "./components/CreateGroup.vue";
+import Gantt from "./components/Gantt.vue";
+import GanttRow from "./components/GanttRow.vue";
+import MemberList from "./components/MemberList.vue";
+import SortableLink from "./components/Sortable.vue";
+import Favorites from "./components/Favorites.vue";
+import GroupTitle from "./components/GroupTitle.vue";
+import FolderWidget from "./components/FolderWidget.vue";
+import AppHeader from "./cla-vue-template/src/components/AppHeader.vue";
+import NavbarItem from "./cla-vue-template/src/components/NavbarItem.vue";
+import AppFooter from "./cla-vue-template/src/components/AppFooter.vue";
+import PostIt from "./cla-vue-template/src/components/PostIt.vue";
 
-import Vue from 'vue'
+import "vue-tour/dist/vue-tour.css";
 
 window.Vue = Vue;
 
-import Vuex from 'vuex'
-Vue.use(Vuex)
-
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
-
-import VueSelect from 'vue-select';
-Vue.component('v-select', VueSelect)
+Vue.use(Vuex);
+Vue.use(VueRouter);
 Vue.use(VueSelect);
-
-
-import VueTour from 'vue-tour'
-require('vue-tour/dist/vue-tour.css')
-Vue.use(VueTour)
-
-
-import Permissions from './mixins/permissions';
+Vue.use(VueTour);
 Vue.mixin(Permissions);
-
-import VueMoment from 'vue-moment';
 Vue.use(VueMoment);
-
-import VTooltip from 'v-tooltip'
 Vue.use(VTooltip);
-
-import Treeselect from '@riophae/vue-treeselect'
-Vue.component('treeselect', Treeselect)
 Vue.use(Treeselect);
 
-import Autocomplete from 'vuejs-auto-complete'
-Vue.component('autocomplete', Autocomplete);
-
-import JsonCSV from 'vue-json-csv'
-Vue.component('downloadCsv', JsonCSV);
-
-import UserHome from './components/UserHome.vue';
-Vue.component('userhome', UserHome);
-
-import LandingPage from './components/LandingPage.vue';
-Vue.component('landingpage', LandingPage);
-
-
-import UserList from './components/UserList.vue';
-Vue.component('userlist', UserList);
-
-
-import Group from './components/Group.vue';
-Vue.component('group', Group);
-
-import Role from './components/Role.vue';
-Vue.component('role', Role);
-
-
-import GroupList from './components/GroupList.vue';
-Vue.component('grouplist', GroupList);
-
-import RoleList from './components/RoleList.vue';
-import Axios from 'axios';
-Vue.component('roleList', GroupList);
-
-
-import ReportList from './components/ReportList.vue';
-Vue.component('reportList', ReportList);
-
-import MissingOfficial from './components/MissingOfficial.vue';
-Vue.component('missingOfficial', MissingOfficial);
-
-import LastModified from './components/LastModified.vue';
-Vue.component('lastModified', LastModified);
-
-import CeddLike from './components/ceddLike.vue';
-Vue.component('ceddLike', CeddLike);
-
-import orgp from './components/orgp.vue';
-Vue.component('orgp', orgp);
-
-
-Vue.component('home', require('./components/Home.vue').default);
-Vue.component('viewuser', require('./components/ViewUser.vue').default);
-Vue.component('viewgroup', require('./components/ViewGroup.vue').default);
-Vue.component('editgroup', require('./components/EditGroup.vue').default);
-Vue.component('members', require('./components/Members.vue').default);
-Vue.component('roles', require('./components/Roles.vue').default);
-Vue.component('modal', require('./components/Modal.vue').default);
-Vue.component('userlookup', require('./components/UserLookup.vue').default);
-Vue.component('creategroup', require('./components/CreateGroup.vue').default);
-Vue.component('gantt', require('./components/Gantt.vue').default);
-Vue.component('gantt-row', require('./components/GanttRow.vue').default);
-Vue.component('member-list', require('./components/MemberList.vue').default);
-Vue.component('sortableLink', require('./components/Sortable.vue').default);
-Vue.component('favorites', require('./components/Favorites.vue').default);
-Vue.component('group-title', require('./components/GroupTitle.vue').default);
-Vue.component('folder-widget', require('./components/FolderWidget.vue').default);
-
-Vue.component('app-header', require('./cla-vue-template/src/components/AppHeader.vue').default);
-Vue.component('navbar-item', require('./cla-vue-template/src/components/NavbarItem.vue').default);
-Vue.component('app-footer', require('./cla-vue-template/src/components/AppFooter.vue').default);
-Vue.component('postit', require('./cla-vue-template/src/components/PostIt.vue').default);
+Vue.component("v-select", VueSelect);
+Vue.component("treeselect", Treeselect);
+Vue.component("autocomplete", Autocomplete);
+Vue.component("downloadCsv", JsonCSV);
+Vue.component("userhome", UserHome);
+Vue.component("landingpage", LandingPage);
+Vue.component("userlist", UserList);
+Vue.component("group", Group);
+Vue.component("role", Role);
+Vue.component("grouplist", GroupList);
+Vue.component("roleList", GroupList);
+Vue.component("reportList", ReportList);
+Vue.component("missingOfficial", MissingOfficial);
+Vue.component("lastModified", LastModified);
+Vue.component("ceddLike", CeddLike);
+Vue.component("orgp", orgp);
+Vue.component("home", Home);
+Vue.component("viewuser", ViewUser);
+Vue.component("viewgroup", ViewGroup);
+Vue.component("editgroup", EditGroup);
+Vue.component("members", Members);
+Vue.component("roles", Roles);
+Vue.component("modal", Modal);
+Vue.component("userlookup", UserLookup);
+Vue.component("creategroup", CreateGroup);
+Vue.component("gantt", Gantt);
+Vue.component("gantt-row", GanttRow);
+Vue.component("member-list", MemberList);
+Vue.component("sortableLink", SortableLink);
+Vue.component("favorites", Favorites);
+Vue.component("group-title", GroupTitle);
+Vue.component("folder-widget", FolderWidget);
+Vue.component("app-header", AppHeader);
+Vue.component("navbar-item", NavbarItem);
+Vue.component("app-footer", AppFooter);
+Vue.component("postit", PostIt);
 
 const store = new Vuex.Store({
   state: {
     favorites: {
       groups: [],
-      roles: []
+      roles: [],
     },
-    user: null
+    user: null,
   },
   actions: {
-    toggleFavorite({commit, state}, payload) {
+    toggleFavorite({ commit, state }, payload) {
       var type = payload.type;
       var item = payload.item;
-      if (state.favorites[type].filter(f => f.id == item.id).length > 0) {
-        axios.delete('/api/user/favorite/' + type + '/' + item.id)
-        .then(response => {
-          commit('removeFavorite', payload);
-        });
-      }
-      else {
-        axios.post('/api/user/favorite/' + type + '/' + item.id)
-            .then(response => {
-                commit('addFavorite', payload);
-            });
+      if (state.favorites[type].filter((f) => f.id == item.id).length > 0) {
+        axios
+          .delete("/api/user/favorite/" + type + "/" + item.id)
+          .then((response) => {
+            commit("removeFavorite", payload);
+          });
+      } else {
+        axios
+          .post("/api/user/favorite/" + type + "/" + item.id)
+          .then((response) => {
+            commit("addFavorite", payload);
+          });
       }
     },
     fetchUser(context) {
-      axios.get("/api/user/show")
-      .then(response => {
-        context.commit('setUser', response.data);
-        context.commit('setFavorites', {type: "groups", content: response.data.favoriteGroups});
-        context.commit('setFavorites', {
-                    type: "roles",
-                    content: response.data.favoriteRoles});
+      axios.get("/api/user/show").then((response) => {
+        context.commit("setUser", response.data);
+        context.commit("setFavorites", {
+          type: "groups",
+          content: response.data.favoriteGroups,
+        });
+        context.commit("setFavorites", {
+          type: "roles",
+          content: response.data.favoriteRoles,
+        });
       });
-    }
+    },
   },
   mutations: {
     addFavorite(state, payload) {
       state.favorites[payload.type].push(payload.item);
     },
     removeFavorite(state, payload) {
-      state.favorites[payload.type] = state.favorites[payload.type].filter(f => f.id != payload.item.id);
+      state.favorites[payload.type] = state.favorites[payload.type].filter(
+        (f) => f.id != payload.item.id
+      );
     },
     setFavorites(state, payload) {
       state.favorites[payload.type] = payload.content;
     },
     setUser(state, payload) {
-      state.user = payload
-    }
-  }
+      state.user = payload;
+    },
+  },
 });
-
-
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -171,28 +163,69 @@ const store = new Vuex.Store({
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-
 const router = new VueRouter({
-    mode: 'history',
+  mode: "history",
   routes: [
-    { name: 'home', path: "/", component: LandingPage },
-    { name: 'user', path: "/user/:userId?", component: UserHome, props:true },
-    { name: 'userList', path: "/userList/", component: UserList, props: (route) => ({ users: route.query.users, groupId:route.query.groupId })},
-    { name: 'group', path: "/group/:groupId/:hash?", component: Group, props:true },
-    { name: 'role', path: "/role/:roleId", component: Role, props:true },
-    { name: 'groupList', path: "/groups/:parent?", component: GroupList, props:true },
-    { name: 'roleList', path: "/roles/", component: RoleList, props:true },
-    { name: 'reportList', path: "/reports/", component: ReportList, props:true },
-    { name: 'missingOfficial', path: "/reports/missingOfficialRoles", component: MissingOfficial, props:true },
-    { name: 'lastModified', path: "/reports/lastModified", component: LastModified, props:true },
-    { name: 'ceddLike', path: "/reports/ceddLike", component: CeddLike, props:true },
-    { name: 'orgp', path: "/reports/orgp", component: orgp, props:true },
-  ]
-})
-
+    { name: "home", path: "/", component: LandingPage },
+    {
+      name: "user",
+      path: "/user/:userId?",
+      component: UserHome,
+      props: true,
+    },
+    {
+      name: "userList",
+      path: "/userList/",
+      component: UserList,
+      props: (route) => ({
+        users: route.query.users,
+        groupId: route.query.groupId,
+      }),
+    },
+    {
+      name: "group",
+      path: "/group/:groupId/:hash?",
+      component: Group,
+      props: true,
+    },
+    { name: "role", path: "/role/:roleId", component: Role, props: true },
+    {
+      name: "groupList",
+      path: "/groups/:parent?",
+      component: GroupList,
+      props: true,
+    },
+    { name: "roleList", path: "/roles/", component: RoleList, props: true },
+    {
+      name: "reportList",
+      path: "/reports/",
+      component: ReportList,
+      props: true,
+    },
+    {
+      name: "missingOfficial",
+      path: "/reports/missingOfficialRoles",
+      component: MissingOfficial,
+      props: true,
+    },
+    {
+      name: "lastModified",
+      path: "/reports/lastModified",
+      component: LastModified,
+      props: true,
+    },
+    {
+      name: "ceddLike",
+      path: "/reports/ceddLike",
+      component: CeddLike,
+      props: true,
+    },
+    { name: "orgp", path: "/reports/orgp", component: orgp, props: true },
+  ],
+});
 
 const app = new Vue({
-    el: '#app',
-    store,
-    router
+  el: "#app",
+  store,
+  router,
 });
