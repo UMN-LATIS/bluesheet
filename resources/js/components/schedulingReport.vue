f7b4bd46c71f1603200d3505aa0bdffe8a820553
Colin McFadden <colin@hedge.video>
2023-07-11 13:27:54 +0530

    make some notes to my self

---
+++ resources/js/components/schedulingReport.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    {{  groupId }}
+
+     this report needs to:
+     1. fetch all of the departmental staff, as well as all of the courses taught over the past 3 years+ anything we know about
+     2. fetch all of the term data for the past 3 years
+     3. fetch all of the leave data for the aforementioned staff
+     4. do that in a performant way
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['groupId'],
+    data() {
+        return {
+
+        }
+    },
+    async mounted() {
+
+        
+
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+
+</script>
