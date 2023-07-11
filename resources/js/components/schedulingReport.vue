c8a2072a3ba0a9ff495659e14c7224c7f40ac789
Colin McFadden <colin@hedge.video>
2023-07-11 13:45:58 +0530

    Add some more notes on how we'll do this

--- resources/js/components/schedulingReport.vue
+++ resources/js/components/schedulingReport.vue
@@ -3,7 +3,7 @@
     {{  groupId }}
 
      this report needs to:
-     1. fetch all of the departmental staff, as well as all of the courses taught over the past 3 years+ anything we know about
+     1. fetch all of the courses taught, then fetch all the people who taguht them, for the past three years
      2. fetch all of the term data for the past 3 years
      3. fetch all of the leave data for the aforementioned staff
      4. do that in a performant way
@@ -20,6 +20,28 @@
           acad_career
         FROM
           #{Rails.configuration.asr_warehouse_schema}.cs_ps_term_tbl
+
+
+        This should probably:
+            1) rely on caching at bandaid for course info
+            2) be done server side so we can get some perf benefits from multiple user queries
+
+        return will be...
+
+        user:
+            (user info)
+            courses:[
+                {
+                    courseInfo
+                    semester (or map this into dates?)
+                }    
+            ]
+            leaves: [
+            {
+                leaveinfo
+            }]
+
+        
 
     </div>
 </template>
