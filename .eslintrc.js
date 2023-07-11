0218dc7d96f12bac5750b7ec6b775bac6a557f0f
James Johnson <johnsojr@umn.edu>
2023-07-10 23:38:51 -0500

    add lint rule for undefined components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,56 @@
 module.exports = {
-  globals: {
-    axios: true,
-  },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    // "plugin:vue/vue3-essential", // Use this if you are using Vue.js 3.x.
-    "plugin:vue/essential" // Use this if you are using Vue.js 2.x.
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    globals: {
+        axios: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "vue"],
-  ignorePatterns: ["resources/js/cla-vue-template"],
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "vue/multi-word-component-names": "off",
-    "vue/attribute-hyphenation": ["error", "never"],
-    "vue/v-on-event-hyphenation": ["error", "never"],
-    "vue/component-definition-name-casing": "off",
-    // "vue/component-name-in-template-casing": [
-    //   "warn",
-    //   "PascalCase",
-    //   {
-    //     registeredComponentsOnly: false,
-    //     ignores: ["component", "router-link", "router-view"],
-    //   },
-    // ],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        // "plugin:vue/vue3-essential", // Use this if you are using Vue.js 3.x.
+        "plugin:vue/essential", // Use this if you are using Vue.js 2.x.
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "vue"],
+    ignorePatterns: ["resources/js/cla-vue-template"],
+    rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "vue/multi-word-component-names": "off",
+        "vue/attribute-hyphenation": ["error", "never"],
+        "vue/v-on-event-hyphenation": ["error", "never"],
+        "vue/component-definition-name-casing": "off",
+        "vue/no-undef-components": [
+            "error",
+            {
+                ignorePatterns: ["router-link", 'router-view', 'v-tour'],
+            },
+        ],
+        // "vue/component-name-in-template-casing": [
+        //   "warn",
+        //   "PascalCase",
+        //   {
+        //     registeredComponentsOnly: false,
+        //     ignores: ["component", "router-link", "router-view"],
+        //   },
+        // ],
+    },
 };
