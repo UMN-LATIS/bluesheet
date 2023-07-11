b6c30a2b3a23df02ca6457166f206305658172ed
James Johnson <johnsojr@umn.edu>
2023-07-11 10:22:32 -0500

    swap mix for @vite directive in base blade

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -12,7 +12,7 @@
     rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css"
     integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+  @vite(['resources/js/app.js'])
 </head>
 
 <body>
@@ -31,7 +31,6 @@
       window.Permissions = [];
     @endauth
   </script>
-  <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
