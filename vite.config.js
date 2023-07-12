055d4f588a4c8438c2c0680d99f22171174069f5
James Johnson <johnsojr@umn.edu>
2023-07-12 11:50:25 -0500

    remove compat mode

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,26 @@ export default defineConfig({
     laravel(["resources/assets/js/app.js"]),
     vue({
       template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
+          transformAssetUrls: {
+              // The Vue plugin will re-write asset URLs, when referenced
+              // in Single File Components, to point to the Laravel web
+              // server. Setting this to `null` allows the Laravel plugin
+              // to instead re-write asset URLs to point to the Vite
+              // server instead.
+              base: null,
+
+              // The Vue plugin will parse absolute URLs and treat them
+              // as absolute paths to files on disk. Setting this to
+              // `false` will leave absolute URLs un-touched so they can
+              // reference assets in the public directory as expected.
+              includeAbsolute: false,
           },
-        },
       },
-    }),
+  }),
   ],
   resolve: {
     alias: {
       "@": "/resources/assets/js",
-
-      // temporarily use vue 2 compat build to support vue 3
-      vue: "@vue/compat",
 
       // use vue's runtime compiler to support vue components
       // directly within blade templates
