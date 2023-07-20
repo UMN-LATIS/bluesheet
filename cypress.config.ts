5c791a134f08fbdb89eff49a336e06b14712cf9e
James Johnson <johnsojr@umn.edu>
2023-07-20 14:31:58 -0500

    update cypress from 9.5.1 to 12.17.1 (#26)

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  retries: 2,
+  defaultCommandTimeout: 5000,
+  watchForFileChanges: true,
+  videosFolder: "tests/cypress/videos",
+  screenshotsFolder: "tests/cypress/screenshots",
+  fixturesFolder: "tests/cypress/fixture",
+  video: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    baseUrl: "http://localhost",
+    specPattern: "tests/cypress/integration/**/*.{test,spec}.js",
+    supportFile: "tests/cypress/support/index.js",
+  },
+});
