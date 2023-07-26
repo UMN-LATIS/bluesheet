b55ef282c311937e651f22ccc9bb35c3c7c3712d
Colin McFadden <colin@hedge.video>
2023-07-26 11:21:31 -0500

    add other leave types

--- app/Leave.php
+++ app/Leave.php
@@ -11,5 +11,10 @@ class Leave extends Model
     const STATUS_CONFIRMED = 'confirm';
     const STATUS_PENDING = 'pending';
     const STATUS_CANCELLED = 'cancelled';
-    const TYPE_SABITICAL = 'sabitical';
+    const TYPE_SABBATICAL = 'sabbatical';
+    const TYPE_DEVELOPMENT = 'development';
+    const TYPE_SINGLE_SEMESTER = 'single_semester';
+    const TYPE_COURSE_RELEASE = 'course_release';
+    const TYPE_COURSE_BUYOUT = 'course_buyout';
+    const TYPE_OTHER = 'other';
 }
