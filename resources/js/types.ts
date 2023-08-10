5260c178e37afc5b3edc79ac40906ccbda3f2cb6
James Johnson <johnsojr@umn.edu>
2023-08-10 13:56:33 -0500

    start typing api responses
    okay. I've spent enought time looking this up, so let's document this someplace so we can use this with typescript.

--- resources/js/types.ts
+++ resources/js/types.ts
@@ -7,6 +7,154 @@ export interface UserLookupItem {
   umndid: string; // "wx74jdbk"
 }
 
+export type ISODateTime = string; // "2023-08-04T19:52:04.000000Z"
+export type ISODate = string; // "2023-08-04"
+
+export interface User {
+  id: number;
+  givenname: string;
+  surname: string;
+  displayName: string;
+  email: string;
+  umndid?: string;
+  emplid?: number;
+  office: string | null;
+  phone?: string | null;
+  title: string | null;
+  ou: string | null;
+  memberships: [];
+  favoriteGroups: [];
+  favoriteRoles: [];
+  seen_tour?: 1 | 0;
+  send_email_reminders: 1 | 0;
+  notify_of_favorite_changes: 1 | 0;
+  leaves?: Leave[];
+  created_at: ISODateTime;
+  updated_at: ISODateTime;
+  deleted_at: ISODateTime | null;
+}
+
+export interface MembershipWithGroups {
+  id: number;
+  filtered: false; // what does this do? Is it a front-end state thing? Does it need to be sticky?
+  user: User;
+  role: MemberRole;
+  start_date: ISODate; // can this be null?
+  end_date: ISODate | null;
+  admin: 1 | 0;
+  notes: null;
+}
+
+export interface GroupType {
+  id: number;
+  label: string; // "ACademic Department"
+  pivot?: {
+    role_id: number;
+    group_type_id: number;
+  };
+}
+
+export interface MemberRole {
+  id: number;
+  label: string; // "Academic Chair"
+  created_at: ISODateTime | null; // why null?
+  updated_at: ISODateTime | null; // why null?
+  deleted_at?: ISODateTime | null;
+  official_role_category_id: number;
+  official_group_type: GroupType[];
+}
+
+export interface ParentOrganization {
+  // group folder?
+  id: number;
+  group_title: string;
+  parent_organization_id: number;
+}
+
+export interface Group {
+  id: number;
+  user_can_edit: boolean; // current user can edit
+  group_title: string | null; // "Anthropology";
+  abbreviation: string | null; // "ANTH";
+  group_type_id?: number;
+  dept_id: string | null;
+  group_type: GroupType;
+  private_group: 0 | 1;
+  parent_group_id: number | null;
+  parent_group: Group | null;
+  child_groups?: ChildGroup[]; // how deep will this go?
+  google_group: string | null;
+  show_unit: 0 | 1;
+  secret_hash: string;
+  parent_organization: ParentOrganization;
+  parent_organization_id: number;
+  active: 0 | 1;
+  artifacts: Artifact[]; // what's an artifact?
+  notes: string | null;
+}
+
+export interface ChildGroup {
+  id: number;
+  group_title: string;
+  group_type_id: number;
+  private_group: 0 | 1;
+  active_group: 0 | 1;
+  start_date: ISODate | null;
+  end_date: ISODate | null;
+  created_at: ISODateTime;
+  updated_at: ISODateTime;
+  deleted_at: ISODateTime | null;
+  parent_organization_id: number; // can this be null?
+  google_group: string | null;
+  show_unit: 0 | 1;
+  parent_group_id: number;
+  abbreviation: string | null;
+  dept_id: string | null;
+  notes: string | null;
+}
+
+export interface Artifact {
+  id: number;
+  label: string;
+  target: string;
+  group_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export const leaveTypes = {
+  SABBATICAL: "sabbatical",
+  DEVELOPMENT: "development",
+  SINGLE_SEMESTER: "single_semester",
+  COURSE_RELEASE: "course_release",
+  COURSE_BUYOUT: "course_buyout",
+  OTHER: "other",
+} as const;
+
+export const leaveStatuses = {
+  CONFIRMED: "confirm",
+  PENDING: "pending",
+  CANCELLED: "cancelled",
+} as const;
+
+export interface Leave {
+  id: number;
+  user_id: number;
+  description: string;
+  type: keyof typeof leaveTypes;
+  status: keyof typeof leaveStatuses;
+  start_date: ISODate;
+  end_date: ISODate;
+  created_at: ISODateTime;
+  updated_at: ISODateTime;
+  deleted_at: ISODateTime | null;
+}
+
+// api response types
+export type ApiUserResponse = User;
+export type ApiGroupMembersReponse = MembershipWithGroups[];
+export type ApiGroupRolesResponse = MemberRole[];
+export type ApiGroupResponse = Group;
 export interface ApiUserLookupResponse {
   items: UserLookupItem[];
 }
