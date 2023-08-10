dc94bfc246c0be51538141d97859f6eb1cba1bd1
James Johnson <johnsojr@umn.edu>
2023-08-10 13:57:05 -0500

    add `getUser` api method

--- resources/js/api.ts
+++ resources/js/api.ts
@@ -1,9 +1,19 @@
 import { axios } from "@/lib";
-import { ApiUserLookupResponse, UserLookupItem } from "@/types";
+import {
+  ApiUserLookupResponse,
+  ApiUserResponse,
+  UserLookupItem,
+  User,
+} from "@/types";
 
 export async function lookupUsers(query: string): Promise<UserLookupItem[]> {
   const res = await axios.get<ApiUserLookupResponse>(
     `/api/autocompleter/user?searchType=nameAndInternetId&q=${query}`,
   );
   return res.data.items;
+}
+
+export async function getUser(userId: number): Promise<User> {
+  const res = await axios.get<ApiUserResponse>(`/api/user/${userId}`);
+  return res.data;
 }
