21838d56613228cd9a51fe29789aec2ddf2b4c6a
James Johnson <johnsojr@umn.edu>
2023-08-12 15:53:42 -0500

    remove unused components
    These were from dead-ends when exploring design/ux directions with leaves.

--- resources/js/icons/index.ts
+++ resources/js/icons/index.ts
@@ -1,9 +1,5 @@
 export { default as CheckIcon } from "./CheckIcon.vue";
 export { default as ChevronDownIcon } from "./ChevronDownIcon.vue";
-export { default as CirclePlusIcon } from "./CirclePlusIcon.vue";
-export { default as CircleMinusIcon } from "./CircleMinusIcon.vue";
 export { default as CircleXIcon } from "./CircleXIcon.vue";
 export { default as ExternalLinkIcon } from "./ExternalLinkIcon.vue";
-export { default as PencilIcon } from "./PencilIcon.vue";
-export { default as TrashIcon } from "./TrashIcon.vue";
 export { default as VDotsIcon } from "./VDotsIcon.vue";
