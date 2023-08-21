dd75ad71be8208d006c02730be4825d01109b0db
James Johnson <johnsojr@umn.edu>
2023-08-21 17:15:12 -0500

    allow roles to be passed as query param

--- app/Http/Controllers/SchedulingController.php
+++ app/Http/Controllers/SchedulingController.php
@@ -6,6 +6,7 @@ use \App\Library\Bandaid;
 use App\Group;
 use App\Http\Resources\CourseWithInstructors;
 use App\Library\UserService;
+use Illuminate\Http\Request;
 
 class SchedulingController extends Controller {
 
@@ -21,7 +22,7 @@ class SchedulingController extends Controller {
         return $this->bandaid->getCLATerms();
     }
 
-    public function getDeptCoursesForTerm(int $termId, Group $group) {
+    public function getDeptCoursesForTerm(int $termId, Group $group, Request $request) {
         if (!$termId) {
             return response()->json(['error' => 'No term id provided.'], 400);
         }
@@ -30,17 +31,29 @@ class SchedulingController extends Controller {
             return response()->json(['error' => 'Group does not have a numeric department id.'], 400);
         }
 
+        $validated = $request->validate([
+            'includeRoles' => 'string'
+        ]);
+
+
         $courses = collect(
             $this->bandaid->getDeptScheduleForTerm($group->dept_id, $termId)
         );
 
         $courseWithInstructors = $this->userService
             ->attachInstructorsToCourses($courses)
-            // exclude courses without instructors
             ->filter(function ($course) {
+                // exclude courses without instructors
                 return isset($course->instructor);
             })
             ->values();
+
+        if (isset($validated['includeRoles'])) {
+            $roles = explode(',', $validated['includeRoles']);
+            $courseWithInstructors = $courseWithInstructors->filter(function ($course) use ($roles) {
+                return in_array($course->INSTRUCTOR_ROLE, $roles);
+            });
+        }
 
         return CourseWithInstructors::collection($courseWithInstructors);
     }
