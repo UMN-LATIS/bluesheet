93e7105937fe971cf0392c0aff6b4225c22f111a
James Johnson <johnsojr@umn.edu>
2023-08-21 22:02:59 -0500

    try some icons for status

--- resources/js/components/LeaveChip.vue
+++ resources/js/components/LeaveChip.vue
@@ -1,11 +1,25 @@
 <template>
-  <Chip :color="statusColor"> {{ leave.description }} ({{ leave.type }}) </Chip>
+  <Chip :color="statusColor">
+    <div class="tw-flex tw-gap-1 tw-items-center">
+      <CircleCheckIcon v-if="leave.status === 'confirmed'" title="confirmed" />
+      <QuestionIcon v-if="leave.status === 'pending'" title="pending" />
+      <NoIcon v-if="leave.status === 'cancelled'" title="cancelled" />
+      <span
+        :class="{
+          'tw-line-through': leave.status === 'cancelled',
+        }"
+      >
+        {{ leave.description }} ({{ leave.type }})
+      </span>
+    </div>
+  </Chip>
 </template>
 <script setup lang="ts">
 import { ISODate, Leave, Term, leaveStatuses } from "@/types";
 import { computed } from "vue";
 import dayjs from "dayjs";
 import Chip from "./Chip.vue";
+import { CircleCheckIcon, QuestionIcon, NoIcon } from "@/icons";
 
 const props = defineProps<{
   leave: Leave;
--- resources/js/icons/index.ts
+++ resources/js/icons/index.ts
@@ -1,5 +1,8 @@
 export { default as CheckIcon } from "./CheckIcon.vue";
 export { default as ChevronDownIcon } from "./ChevronDownIcon.vue";
+export { default as CircleCheckIcon } from "./CircleCheckIcon.vue";
 export { default as CircleXIcon } from "./CircleXIcon.vue";
 export { default as ExternalLinkIcon } from "./ExternalLinkIcon.vue";
+export { default as NoIcon } from "./NoIcon.vue";
+export { default as QuestionIcon } from "./QuestionIcon.vue";
 export { default as VDotsIcon } from "./VDotsIcon.vue";
