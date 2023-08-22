a06c65641584772ae18b57ae6c005e524f5f27aa
James Johnson <johnsojr@umn.edu>
2023-08-21 22:18:35 -0500

    click to show details

--- resources/js/components/LeaveChip.vue
+++ resources/js/components/LeaveChip.vue
@@ -1,6 +1,18 @@
 <template>
-  <Chip :color="statusColor">
-    <div class="tw-flex tw-gap-1 tw-items-center">
+  <Chip
+    :color="statusColor"
+    class="tw-cursor-pointer"
+    :class="{
+      'tw-rounded-xl': isOpen,
+    }"
+    @click="isOpen = !isOpen"
+  >
+    <header
+      class="tw-flex tw-gap-1 tw-items-center"
+      :class="{
+        'tw-font-bold': isOpen,
+      }"
+    >
       <CircleCheckIcon v-if="leave.status === 'confirmed'" title="confirmed" />
       <QuestionIcon v-if="leave.status === 'pending'" title="pending" />
       <NoIcon v-if="leave.status === 'cancelled'" title="cancelled" />
@@ -9,14 +21,28 @@
           'tw-line-through': leave.status === 'cancelled',
         }"
       >
-        {{ leave.description }} ({{ leave.type }})
+        {{ leave.type }} Leave
       </span>
+    </header>
+
+    <div
+      v-if="isOpen"
+      class="leave-details tw-flex tw-flex-col tw-gap-2 tw-items-center tw-text-xs"
+    >
+      <ul class="tw-text-xs tw-pl-6 tw-list-none">
+        <li>{{ leave.description }}</li>
+        <li>
+          {{ dayjs(leave.start_date).format("MMM D, YYYY") }} &ndash;
+          {{ dayjs(leave.end_date).format("MMM D, YYYY") }}
+        </li>
+        <li>{{ leave.status }}</li>
+      </ul>
     </div>
   </Chip>
 </template>
 <script setup lang="ts">
 import { ISODate, Leave, Term, leaveStatuses } from "@/types";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import dayjs from "dayjs";
 import Chip from "./Chip.vue";
 import { CircleCheckIcon, QuestionIcon, NoIcon } from "@/icons";
@@ -24,6 +50,8 @@ import { CircleCheckIcon, QuestionIcon, NoIcon } from "@/icons";
 const props = defineProps<{
   leave: Leave;
 }>();
+
+const isOpen = ref(false);
 
 const statusColor = computed(() => {
   switch (props.leave.status) {
