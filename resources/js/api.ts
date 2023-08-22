930ce67ded7b97a6b0ea6781b3006d29ab595348
James Johnson <johnsojr@umn.edu>
2023-08-22 10:47:21 -0500

    cache some api calls

--- resources/js/api.ts
+++ resources/js/api.ts
@@ -56,11 +56,20 @@ export async function updateUserLeaves(userId: number, leaves: Leave[]) {
   return res.data;
 }
 
+let getTermsCache = [] as Term[];
 export async function getTerms() {
+  if (getTermsCache.length) {
+    return getTermsCache;
+  }
   const res = await axios.get<Term[]>(`/api/terms`);
+  getTermsCache = res.data;
   return res.data;
 }
 
+type GroupId = number;
+type TermId = number;
+type GetGroupByTermCacheKey = `${GroupId}-${TermId}`;
+const getGroupByTermCache: Map<GetGroupByTermCacheKey, Course[]> = new Map();
 export async function getGroupCoursesByTerm({
   groupId,
   termId,
@@ -68,13 +77,25 @@ export async function getGroupCoursesByTerm({
   groupId: number;
   termId: number;
 }) {
+  const cacheKey: GetGroupByTermCacheKey = `${groupId}-${termId}`;
+  if (getGroupByTermCache.has(cacheKey)) {
+    return getGroupByTermCache.get(cacheKey)!;
+  }
+
   const res = await axios.get<Course[]>(
     `/api/terms/${termId}/groups/${groupId}/courses?includeRoles=PI`,
   );
+  getGroupByTermCache.set(cacheKey, res.data);
   return res.data;
 }
 
+const getGroupCache: Map<GroupId, Group> = new Map();
 export async function getGroup(groupId: number) {
+  if (getGroupCache.has(groupId)) {
+    return getGroupCache.get(groupId)!;
+  }
+
   const res = await axios.get<Group>(`/api/group/${groupId}`);
+  getGroupCache.set(groupId, res.data);
   return res.data;
 }
