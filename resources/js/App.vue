cb205ddaf51dd570912c8b4c9abe1a6fd8256966
James Johnson <johnsojr@umn.edu>
2023-09-07 09:54:29 -0500

    Strip tour stuff (#51)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -64,7 +64,6 @@
       :show="createGroup"
       @close="createGroup = false"
     />
-    <v-tour name="intro_tour" :steps="steps"></v-tour>
   </div>
 </template>
 
@@ -93,48 +92,11 @@ export default {
     return {
       findUser: false,
       createGroup: false,
-      steps: [
-        {
-          target: "#v-step-0", // We're using document.querySelector() under the hood
-          header: {
-            title: "Welcome to the BlueSheet Tool",
-          },
-          content: `Discover the <strong>BlueSheet Tool</strong> with this quick tour. <br/> We'll show you the basics, and you can always reach out for assistance.`,
-        },
-        {
-          target: "#v-step-1",
-          content: `Right now, you're on the home page. This gives you quick access to some common needs.`,
-        },
-        {
-          target: "#v-step-4",
-          content: `The BlueSheet Tool is used to keep track of the membership of groups, committees, and departments. This includes both the people involved and their roles. If someone has two roles within a group, they'll have two entries in the list.`,
-        },
-        {
-          target: "#pastRoles",
-          content:
-            "One of the powerful features of the BlueSheet Tool is that it keeps track of any previous group memberships as well. As you use the tool, this checkbox will let you view historical data.",
-        },
-        {
-          target: "#v-step-6",
-          content: `You can browse all of the available groups using this tab. Groups are organized into folders, but you'll also be able to search for a specific group. You can favorite groups that you use often, and they'll apear on your "My Groups" page.`,
-        },
-        {
-          target: "#v-step-7",
-          content: `The "Browse Roles" tab allows you to see all of the people who hold a given role. This can be useful for answering questions like "Who are all the Directors of Undergraduate Studies".`,
-        },
-        {
-          target: "#v-step-8",
-          content: `If you've got any questions, the "Help" tab has guides, frequently asked questions, and an intro video. You can also contact us if you've got any questions.`,
-        },
-      ],
     };
   },
   mounted() {
     this.$store.dispatch("fetchUser");
     console.log("Component mounted.");
-    if (window.showTour) {
-      this.$tours["intro_tour"].start();
-    }
   },
   methods: {
     $can,
