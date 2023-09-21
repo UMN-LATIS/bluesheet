e1a86f1f3275ae3cacf986079354e6fdfc495a76
James Johnson <johnsojr@umn.edu>
2023-09-21 17:21:48 -0500

    Fix: autocomplete clicking in safari
    Background:
    When typing in the person search field, the dropdown appears with a list of buttons that are choices to complete the user.
    
    When there's no input focus or there's no focus within (say on a button), the menu should disappear using the css:
    ```
    .person-search:not(:focus-within) .person-search__results {
      display: none;
    }
    ```
    
    However, Safari behaves differently. A button is not considered a focusable element in Safari. So clicking on the button, causes the person-search NOT to have focus within it, which hides the potential results and thus prevents the button from being clicked. (Or at least that's what I think is happening).
    
    I tried giving the buttons a tabindex to explicitly make them focusable, but that didn't work.
    
    So, this reworks the component to use JS to open/close the autocomplete box.
    
    In Chrome and Firefox, this is fine.

--- resources/js/components/PersonSearch.vue
+++ resources/js/components/PersonSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="person-search">
+  <div ref="personSearchContainer" class="person-search">
     <input
       v-bind="$attrs"
       :value="modelValue"
@@ -7,9 +7,13 @@
       class="person-search__input form-control"
       autocomplete="off"
       @input="handleInput"
+      @focus="isMenuOpen = true"
     />
 
-    <div class="person-search__results">
+    <div
+      class="person-search__results"
+      :class="{ 'person-search__results--is-open': isMenuOpen }"
+    >
       <div v-if="!users.length" class="person-search__no-items">
         <span v-if="isFetching">Searching...</span>
         <span v-else-if="modelValue.length < 3">
@@ -23,7 +27,7 @@
         :key="user.umndid"
         class="person-search__item"
         type="button"
-        @click="$emit('selected', user)"
+        @click="handleSelect(user)"
       >
         <span class="person-search__name">{{ user.full_name }}</span>
         <span class="person-search__id">({{ user.uid }})</span>
@@ -36,6 +40,7 @@ import { ref, watch } from "vue";
 import { UserLookupItem, CSSClass } from "@/types";
 import pDebounce from "p-debounce";
 import { lookupUsers } from "@/api";
+import { onClickOutside } from "@vueuse/core";
 
 const props = defineProps<{
   modelValue: string;
@@ -49,6 +54,12 @@ const emit = defineEmits<{
 
 const users = ref<UserLookupItem[]>([]);
 const isFetching = ref(false);
+const isMenuOpen = ref(false);
+const personSearchContainer = ref<HTMLElement | null>(null);
+
+onClickOutside(personSearchContainer, () => {
+  isMenuOpen.value = false;
+});
 
 const debouncedLookupUsers = pDebounce(lookupUsers, 500);
 
@@ -69,6 +80,12 @@ watch(
 function handleInput(event: Event) {
   const query = (event.target as HTMLInputElement).value;
   emit("update:modelValue", query);
+}
+
+function handleSelect(user: UserLookupItem) {
+  isMenuOpen.value = false;
+  emit("update:modelValue", user.full_name);
+  emit("selected", user);
 }
 </script>
 <style scoped>
@@ -100,11 +117,13 @@ function handleInput(event: Event) {
     0 1px 3px 0 rgb(0 0 0 / 0.1),
     0 1px 2px -1px rgb(0 0 0 / 0.1);
   flex-direction: column;
-  display: flex;
 }
 
-.person-search:not(:focus-within) .person-search__results {
+.person-search__results {
   display: none;
+}
+.person-search__results.person-search__results--is-open {
+  display: flex;
 }
 
 .person-search__list {
