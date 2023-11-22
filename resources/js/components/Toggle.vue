c7e660fe527ab3b11b16b622119554f20997b2e7
James Johnson <johnsojr@umn.edu>
2023-11-22 17:34:33 -0600

    descrease toggle size, increase label spacing

--- resources/js/components/Toggle.vue
+++ resources/js/components/Toggle.vue
@@ -4,8 +4,8 @@
       :modelValue="modelValue"
       :class="[
         'tw-p-0',
-        modelValue ? 'tw-bg-indigo-600' : 'tw-bg-gray-200',
-        'tw-relative tw-inline-flex tw-h-6 tw-w-11 tw-flex-shrink-0 tw-cursor-pointer tw-rounded-full tw-border-2 tw-border-transparent tw-transition-colors tw-duration-200 tw-ease-in-out focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-indigo-600 focus:tw-ring-offset-2',
+        modelValue ? 'tw-bg-blue-600' : 'tw-bg-gray-200',
+        'tw-relative tw-inline-flex tw-h-5 tw-w-10 tw-flex-shrink-0 tw-cursor-pointer tw-rounded-full tw-border-2 tw-border-transparent tw-transition-colors tw-duration-200 tw-ease-in-out focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-blue-600 focus:tw-ring-offset-2',
       ]"
       @update:modelValue="$emit('update:modelValue', $event)"
     >
@@ -13,11 +13,11 @@
         aria-hidden="true"
         :class="[
           modelValue ? 'tw-translate-x-5' : 'tw-translate-x-0',
-          'tw-pointer-events-none tw-inline-block tw-h-5 tw-w-5 tw-transform tw-rounded-full tw-bg-white tw-shadow tw-ring-0 tw-transition tw-duration-200 tw-ease-in-out',
+          'tw-pointer-events-none tw-inline-block tw-h-4 tw-w-4 tw-transform tw-rounded-full tw-bg-white tw-shadow tw-ring-0 tw-transition tw-duration-200 tw-ease-in-out',
         ]"
       />
     </Switch>
-    <SwitchLabel as="span" class="tw-whitespace-nowrap tw-ml-1 tw-text-sm">
+    <SwitchLabel as="span" class="tw-whitespace-nowrap tw-ml-1.5 tw-text-sm">
       <slot />
     </SwitchLabel>
   </SwitchGroup>
