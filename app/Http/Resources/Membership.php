73e5b3a49e6e5f25f6042edacd7a670701e84e33
James Johnson <johnsojr@umn.edu>
2023-12-05 15:57:58 -0600

    fix n+1 query - remove load('officialGroupType')
    Not sure what the intent is here, but it seems like it might be unnecessary (routes that I've been looking at already have this info). Maybe not tho?
    
    Whatever the case, this loading shouldn't be done in the resource, but in one batch by the controller.

--- app/Http/Resources/Membership.php
+++ app/Http/Resources/Membership.php
@@ -19,7 +19,7 @@ class Membership extends JsonResource
             "filtered"=>false,
             "id"=>$this->id,
             "user"=>$this->user,
-            "role"=>$this->role->load("officialGroupType"),
+            "role"=>$this->role,
             "start_date"=>$this->start_date?$this->start_date->format('Y-m-d'):null,
             "end_date"=>$this->end_date?$this->end_date->format('Y-m-d'):null,
             "admin"=>$this->admin,
