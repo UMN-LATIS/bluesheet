f824140c749633045185db0ce8d539a7cce394ba
James Johnson <johnsojr@umn.edu>
2023-12-05 17:47:31 -0600

    scroll to current term

--- resources/js/features/course-planning/CoursePlanningPage.vue
+++ resources/js/features/course-planning/CoursePlanningPage.vue
@@ -1,7 +1,7 @@
 <template>
   <WideLayout>
     <Transition name="fade" mode="out-in">
-      <div v-if="isReady">
+      <div v-if="isLoadingComplete">
         <h1 class="tw-mb-4">
           {{ group?.group_title }} <br />
           <span class="tw-text-3xl">Scheduling Report</span>
@@ -10,6 +10,7 @@
         <CoursePlanningFilters :groupId="props.groupId" />
 
         <InstructorTable
+          ref="tableRef"
           label="Instructors"
           :groupId="props.groupId"
           :roles="['PI']"
@@ -28,7 +29,7 @@
 <script setup lang="ts">
 import WideLayout from "@/layouts/WideLayout.vue";
 import { onMounted } from "vue";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import InstructorTable from "./components/InstructorTable.vue";
 import { useRootCoursePlanningStore } from "./stores/useRootCoursePlanningStore";
 import CoursePlanningFilters from "./components/CoursePlanningFilters.vue";
@@ -39,14 +40,41 @@ const props = defineProps<{
 }>();
 
 const coursePlanningStore = useRootCoursePlanningStore();
-const isReady = ref(false);
+const isLoadingComplete = ref(false);
 
 onMounted(async () => {
   await coursePlanningStore.initGroup(props.groupId);
   console.log("group initialized");
-  isReady.value = true;
+  isLoadingComplete.value = true;
 });
 
 const group = computed(() => coursePlanningStore.getGroup(props.groupId));
+const currentTerm = computed(() => coursePlanningStore.currentTerm);
+
+const tableRef = ref<HTMLElement>();
+watch(
+  [isLoadingComplete, tableRef],
+  () => {
+    if (!isLoadingComplete.value || !tableRef.value) return;
+    scrollToCurrentTerm();
+  },
+  { immediate: true },
+);
+
+function scrollToCurrentTerm() {
+  if (!currentTerm.value) return;
+  const currentTermEl = document.getElementById(`term-${currentTerm.value.id}`);
+  if (!currentTermEl) {
+    console.warn(
+      `Could not find element for current term ${currentTerm.value.name}`,
+    );
+    return;
+  }
+  currentTermEl.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
+}
 </script>
 <style scoped></style>
