a00b91b0e1db9d6582739394d8042fa7651c4739
James Johnson <johnsojr@umn.edu>
2023-12-11 01:00:00 -0600

    only show edit tools in planning mode

--- resources/js/pages/CoursePlanningPage/components/PersonTable/UnpublishedSectionDetails.vue
+++ resources/js/pages/CoursePlanningPage/components/PersonTable/UnpublishedSectionDetails.vue
@@ -1,8 +1,12 @@
 <template>
   <div
-    class="tw-bg-blue-50 tw-shadow tw-py-2 tw-flex tw-gap-1 tw-cursor-move tw-items-top"
+    class="tw-bg-neutral-100 tw-py-2 tw-flex tw-gap-1 tw-cursor-move tw-items-top tw-italic"
+    :class="{
+      'tw-cursor-move tw-shadow': isInPlanningMode,
+      'tw-cursor-default tw-px-2': !isInPlanningMode,
+    }"
   >
-    <DragHandleIcon class="tw-inline-block" />
+    <DragHandleIcon v-if="isInPlanningMode" class="tw-inline-block" />
     <div class="tw-flex-1 tw-overflow-hidden">
       <h2 class="tw-text-sm tw-m-0">
         {{ course.subject }} {{ course.catalogNumber }}
@@ -11,7 +15,7 @@
         {{ course.title }}
       </p>
     </div>
-    <MoreMenu>
+    <MoreMenu v-if="isInPlanningMode">
       <MoreMenuItem
         class="tw-flex tw-gap-2 tw-items-center"
         @click="handleEdit"
@@ -26,9 +30,8 @@
 </template>
 <script setup lang="ts">
 import * as T from "@/types";
-import { DragHandleIcon, PencilIcon } from "@/icons";
+import { DragHandleIcon } from "@/icons";
 import { MoreMenu, MoreMenuItem } from "@/components/MoreMenu";
-import XIcon from "@/icons/XIcon.vue";
 import { useRootCoursePlanningStore } from "../../stores/useRootCoursePlanningStore";
 import { computed } from "vue";
 
@@ -42,6 +45,10 @@ const planningStore = useRootCoursePlanningStore();
 const enrollment = computed(() =>
   planningStore.getEnrollmentForPersonInSection(props.person, props.section),
 );
+
+const isInPlanningMode = computed(() => {
+  return planningStore.isInPlanningMode && !props.section.isPublished;
+});
 
 function handleEdit() {
   console.log("edit");
