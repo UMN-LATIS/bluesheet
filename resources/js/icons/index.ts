8afe8a3315dc47abd3b6092ddfa26688bf672795
James Johnson <johnsojr@umn.edu>
2023-12-21 21:13:34 -0600

    add chevron right icon

--- resources/js/icons/index.ts
+++ resources/js/icons/index.ts
@@ -1,7 +1,9 @@
 export { default as CheckIcon } from "./CheckIcon.vue";
 export { default as ChevronUpDownIcon } from "./ChevronUpDownIcon.vue";
 export { default as ChevronDownIcon } from "./ChevronDownIcon.vue";
+export { default as ChevronRightIcon } from "./ChevronRightIcon.vue";
 export { default as CircleCheckIcon } from "./CircleCheckIcon.vue";
+
 export { default as CircleXIcon } from "./CircleXIcon.vue";
 export { default as DragHandleIcon } from "./DragHandleIcon.vue";
 export { default as ExternalLinkIcon } from "./ExternalLinkIcon.vue";
