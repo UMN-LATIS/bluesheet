afb7cceb65c6d0663d4ab633d633685817794c7a
James Johnson <johnsojr@umn.edu>
2024-02-06 16:53:36 -0600

    change to Laravel Http from Guzzle
    We can use `Http::fake` to mock bandaid api responses in tests, but we're not using Laravel's `Http` client here.
    
    This swaps out Guzzle for Http.
    
    Also, moves the BANDAID_BASE_URI to the bandaid config with the key.

--- config/bandaid.php
+++ config/bandaid.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-  'key' => env('BANDAID_KEY', ''),
+    'key' => env('BANDAID_KEY', ''),
+    'baseUri' => env('BANDAID_BASE_URI', 'https://cla-bandaid-prd-web.oit.umn.edu/api/'),
 ];
