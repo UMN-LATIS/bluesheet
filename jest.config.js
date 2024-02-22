763bb1f1f17c3b980ce6f3ffcfdb2ea1690a61a9
James Johnson <johnsojr@umn.edu>
2024-02-22 16:04:51 -0600

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+  testEnvironment: "jsdom",
   testMatch: [
     "**/resources/js/**/__tests__/**/*.?(m)[jt]s?(x)",
     "**/resources/js/**/?(*.)+(spec|test).?(m)[tj]s?(x)",
   ],
   preset: "ts-jest/presets/js-with-ts-esm",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/resources/js/$1",
+    "^lodash-es$": "lodash",
+  },
 };
