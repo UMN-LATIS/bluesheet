b05111ab3640cb0491c1e81b17b8bbb7b9082bb9
James Johnson <johnsojr@umn.edu>
2024-03-14 13:36:47 -0500

    add relationship between group and leaves
    for the leaves planning report we want to get the leaves for all group members

--- app/Group.php
+++ app/Group.php
@@ -102,4 +102,11 @@ class Group extends Model implements AuditableContract {
     public function courses() {
         return $this->hasMany(Course::class);
     }
+
+    /**
+     * Get the leaves for all group members
+     */
+    public function leaves() {
+        return $this->through('users')->has('leaves');
+    }
 }
