8d84c901efebd70e8f42140423c7874b620adfdb
James Johnson <johnsojr@umn.edu>
2024-03-15 09:44:07 -0500

    remove debugbar stuff

--- app/Library/UserService.php
+++ app/Library/UserService.php
@@ -111,7 +111,6 @@ class UserService {
             return $cachedInstructors;
         }
 
-        \Debugbar::startMeasure('getDeptInstructors');
         $deptCourses = $this->bandaid->getDeptClassList($deptId);
         $allDeptEmplids = collect($deptCourses)
             ->pluck('INSTRUCTOR_EMPLID')
@@ -119,21 +118,13 @@ class UserService {
             ->filter()
             ->values()
             ->toArray();
-        \Debugbar::stopMeasure('getDeptInstructors');
-
-        \Debugbar::startMeasure('getEmployees');
 
         // get employee info from bandaid for job code and category
         // note: only active employees will have a job code
         $activeDeptEmployees = $this->bandaid->getEmployees($allDeptEmplids);
 
-        \Debugbar::stopMeasure('getEmployees');
-
-        \Debugbar::startMeasure('keyByEmplid');
         $activeDeptEmployeeLookup = collect($activeDeptEmployees)->keyBy('EMPLID');
-        \Debugbar::stopMeasure('keyByEmplid');
 
-        \Debugbar::startMeasure('findOrCreateManyByEmplId');
         $instructors = $this
             ->findOrCreateManyByEmplId($allDeptEmplids)
             ->map(function ($user) use ($activeDeptEmployeeLookup) {
@@ -144,7 +135,6 @@ class UserService {
                 return $user;
             })
             ->values();
-        \Debugbar::stopMeasure('findOrCreateManyByEmplId');
 
         Cache::put($cacheKey, $instructors, now()->addMinutes(10));
 
