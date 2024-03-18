9ac83966fd7dd1d20e2e65d70639962352705f2b
James Johnson <johnsojr@umn.edu>
2024-03-18 13:16:17 -0500

    Add "Leave impersonation" button to top of page when impersonation (#125)

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,14 +13,40 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css"
     integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
   @vite(['resources/js/app.js'])
+
+  <style>
+    .impersonation-header {
+      background-color: #c42da8;
+      color: white;
+    }
+
+    .impersonation-header__inner {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      max-width: 90rem;
+      padding: 0.5rem 1rem;
+      margin: 0 auto;
+    }
+  </style>
 </head>
 
 <body>
+  @impersonating($guard = null)
+    <div class="impersonation-header">
+      <div class="impersonation-header__inner">
+        <div>{{ auth()->user()->displayName }}</div>
+
+        <div>
+          <a class="btn btn-outline-light" href="{{ route('impersonate.leave') }}">Leave impersonation</a>
+        </div>
+      </div>
+    </div>
+  @endImpersonating
+
+
   @yield('content')
 
-  @impersonating
-    <a class="" href="/impersonate/leave">End Impersonation</a>
-  @endImpersonating
   @yield('footer')
 
   <script>
