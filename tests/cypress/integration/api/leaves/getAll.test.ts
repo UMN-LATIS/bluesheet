192b33ae78ca78c158301eca9f4ba811c131fa9a
James Johnson <johnsojr@umn.edu>
2024-04-01 16:51:35 -0500

    test fix

--- tests/cypress/integration/api/leaves/getAll.test.ts
+++ tests/cypress/integration/api/leaves/getAll.test.ts
@@ -18,27 +18,9 @@ describe("GET /api/remote/leaves", () => {
     });
   });
 
-  it("returns a 401 for a non-wildcard token", () => {
+  it("returns a 403 for a non-wildcard token", () => {
     cy.php(
       `App\\User::where('umndid', 'basic_user')->firstOrFail()->createToken('test',['test_ability'])`,
-    ).then((token) => {
-      cy.request({
-        method: "GET",
-        url: "/api/remote/leaves",
-        headers: {
-          Authorization: `Bearer ${token.plaintexttoken}`,
-          Accept: "application/json",
-        },
-        failOnStatusCode: false,
-      }).then((response) => {
-        expect(response.status).to.eq(401);
-      });
-    });
-  });
-
-  it.only("returns a list of leaves for a super admin", () => {
-    cy.php(
-      `App\\User::where('umndid', 'admin')->firstOrFail()->createToken('test')`,
     )
       .then((token) => {
         return cy.request({
@@ -48,21 +30,41 @@ describe("GET /api/remote/leaves", () => {
             Authorization: `Bearer ${token.plainTextToken}`,
             Accept: "application/json",
           },
+          failOnStatusCode: false,
         });
       })
       .then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body.length).to.be.greaterThan(0);
-        expect(response.body[0]).to.have.keys([
-          "id",
-          "description",
-          "start_date",
-          "end_date",
-          "type",
-          "status",
-          "user",
-          "terms",
-        ]);
+        expect(response.status).to.eq(403);
       });
   });
+});
+
+it("returns a list of leaves for a super admin", () => {
+  cy.php(
+    `App\\User::where('umndid', 'admin')->firstOrFail()->createToken('test')`,
+  )
+    .then((token) => {
+      return cy.request({
+        method: "GET",
+        url: "/api/remote/leaves",
+        headers: {
+          Authorization: `Bearer ${token.plainTextToken}`,
+          Accept: "application/json",
+        },
+      });
+    })
+    .then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.length).to.be.greaterThan(0);
+      expect(response.body[0]).to.have.keys([
+        "id",
+        "description",
+        "start_date",
+        "end_date",
+        "type",
+        "status",
+        "user",
+        "terms",
+      ]);
+    });
 });
