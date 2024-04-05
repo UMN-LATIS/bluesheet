83e3cd366ae8348dc8f742932a8525b567b60ee7
James Johnson <johnsojr@umn.edu>
2024-04-05 14:40:52 -0500

    test that manager can get leaves for instructors in their dept

--- tests/Feature/api/users/leaves/GetAllUserLeavesTest.php
+++ tests/Feature/api/users/leaves/GetAllUserLeavesTest.php
@@ -5,6 +5,8 @@ use App\Leave;
 use App\Membership;
 use App\User;
 use Database\Seeders\TestDatabaseSeeder;
+use Illuminate\Support\Facades\Http;
+
 use function Pest\Laravel\{postJson, getJson, actingAs, deleteJson};
 
 beforeEach(function () {
@@ -83,6 +85,103 @@ it('does not allow a user to view leaves for a user that does not exist', functi
         ->assertNotFound();
 });
 
+it('lets BlueSheet Managers view leaves of any instructors in their group\'s department', function () {
+    // create a membership for the BlueSheet Manager
+    // which will create the group and the user
+    $blueSheetManagerMembership = Membership::factory()->create();
+    $blueSheetManager = $blueSheetManagerMembership->user;
+    $blueSheetManagerGroup = $blueSheetManagerMembership->group;
+
+    // promote the BlueSheet Manager to a group manager
+    promoteUserToGroupManager($blueSheetManager->id, $blueSheetManagerGroup->id);
+
+    // give our group a dept_id
+    $blueSheetManagerGroup->dept_id = 123;
+    $blueSheetManagerGroup->save();
+
+    // our mock Bandaid getDeptClassList(123)
+    // will return classes for an instructor with emplid
+    // of 102
+    // so our bluesheet manager should be able to see
+    // any leaves for user with emplid 102
+
+    // let's give them a leave
+    $instructor = User::factory()->create(['emplid' => 102]);
+    $leave = Leave::factory()->create(['user_id' => $instructor->id]);
+
+    // now verify that the BlueSheet Manager can see the leave
+    actingAs($blueSheetManager)
+        ->getJson("/api/users/{$instructor->id}/leaves")
+        ->assertOk()
+        ->assertJsonCount(1);
+});
+
+it('does not let BlueSheet Managers view leaves of instructors outside their group\'s department', function () {
+    // create a membership for the BlueSheet Manager
+    // which will create the group and the user
+    $managerMembership = Membership::factory()->create();
+
+    // promote user to group manager
+    promoteUserToGroupManager($managerMembership->user_id, $managerMembership->group_id);
+
+    $manager = $managerMembership->user;
+    $group = $managerMembership->group;
+    $deptId = $group->dept_id;
+
+
+    // create an instructor who will be in our
+    // dept but not in our group
+    $instructor1 = User::factory()->create();
+
+    // create an instructor who will not be in our department
+    $instructor2 = User::factory()->create();
+
+    // clear any existing fake HTTP responses
+    // so that we can set up a new Bandaid API response
+    clearExistingHttpFakes();
+
+    $BANDAID_API = config('bandaid.baseUri');
+    Http::fake([
+        // Now, put the $instructor1 in the mock
+        // reponse for getEmployeesForDept($deptId)
+        "{$BANDAID_API}/classes/list/{$deptId}" => Http::response([
+            ['INSTRUCTOR_EMPLID' => $instructor1->emplid]
+        ]),
+
+        // and the $instructor2 in the mock response
+        // for getEmployeesForDept($deptId + 1)
+        "{$BANDAID_API}/classes/list/" . ($deptId + 1) => Http::response([
+            ['INSTRUCTOR_EMPLID' => $instructor2->emplid]
+        ]),
+    ]);
+
+    // finally, let's check that we can see the leaves of the instructor in
+    // our department (this is just a sanity check that we
+    // have set up our test correctly)
+    actingAs($manager)
+        ->getJson("/api/users/{$instructor1->id}/leaves")
+        ->assertOk();
+
+    // and then attempt to get a leave for the instructor not in our departmemt
+    // which should fail
+    actingAs($manager)
+        ->getJson("/api/users/{$instructor2->id}/leaves")
+        ->assertForbidden();
+
+    // but if we change the department of the group to the department
+    // of the instructor, we should be able to see their leaves
+    $group->dept_id = $deptId + 1;
+    $group->save();
+
+    actingAs($manager)
+        ->getJson("/api/users/{$instructor2->id}/leaves")
+        ->assertOk();
+
+    // and now we should no longer be able to get the leaves of the instructor1
+    actingAs($manager)
+        ->getJson("/api/users/{$instructor1->id}/leaves")
+        ->assertForbidden();
+});
 
 describe('as a fellow group member', function () {
     beforeEach(function () {
