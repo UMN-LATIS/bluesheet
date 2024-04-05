946b380332a23826f380ad8881d781338d1f24fc
James Johnson <johnsojr@umn.edu>
2024-04-05 10:33:25 -0500

    move post api tests to pest, organize api tests by endpoint

--- app/Policies/LeavePolicy.php
+++ app/Policies/LeavePolicy.php
@@ -94,20 +94,6 @@ class LeavePolicy {
     }
 
     /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Leave $leave): bool {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Leave $leave): bool {
-        return false;
-    }
-
-    /**
      * Determine whether the current user can view all leaves
      * of another user.
      */
