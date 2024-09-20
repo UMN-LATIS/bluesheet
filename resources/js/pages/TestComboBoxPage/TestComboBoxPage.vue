0cc9915931b1ef034e5e560d7eb42cf2eb300829
James Johnson <johnsojr@umn.edu>
2024-09-19 19:40:28 -0500

    add test page

--- resources/js/pages/TestComboBoxPage/TestComboBoxPage.vue
+++ resources/js/pages/TestComboBoxPage/TestComboBoxPage.vue
@@ -1,9 +1,85 @@
 <template>
   <PostIt class="tw-mb-4">
     <h1>Test Combobox Page</h1>
-    <ComboBox v-model="selectedOption" :options="options" label="test" />
+    <ComboBox
+      v-model="selectedOption"
+      :options="options"
+      label="New ComboBox"
+      class="tw-mb-4"
+    />
 
-    <!-- <OldCombobox v-model="selectedOption" :options="options" label="test" /> -->
+    <OldCombobox
+      v-model="selectedOption"
+      :options="options"
+      label="Old ComboBox"
+      class="tw-mb-4"
+    />
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam labore
+      aliquid sed maxime fuga deleniti alias dolorum! Debitis, mollitia
+      provident repellendus, asperiores aut doloribus deserunt, animi nam ab
+      dolorum itaque.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam labore
+      aliquid sed maxime fuga deleniti alias dolorum! Debitis, mollitia
+      provident repellendus, asperiores aut doloribus deserunt, animi nam ab
+      dolorum itaque.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam labore
+      aliquid sed maxime fuga deleniti alias dolorum! Debitis, mollitia
+      provident repellendus, asperiores aut doloribus deserunt, animi nam ab
+      dolorum itaque.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam labore
+      aliquid sed maxime fuga deleniti alias dolorum! Debitis, mollitia
+      provident repellendus, asperiores aut doloribus deserunt, animi nam ab
+      dolorum itaque.
+    </p>
+
+    <h2>Scrollable Container</h2>
+
+    <div class="tw-h-[200px] tw-overflow-y-auto tw-border tw-p-4 tw-mb-8">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam labore
+        aliquid sed maxime fuga deleniti alias dolorum! Debitis, mollitia
+        provident repellendus, asperiores aut doloribus deserunt, animi nam ab
+        dolorum itaque.
+      </p>
+      <ComboBox
+        v-model="selectedOption"
+        :options="options"
+        label="New ComboBox"
+      />
+
+      <OldCombobox
+        v-model="selectedOption"
+        :options="options"
+        label="Old ComboBox"
+      />
+    </div>
+
+    <Button @click="isModalOpen = true">Open Modal</Button>
+
+    <Modal :show="isModalOpen" @close="isModalOpen = false">
+      <div class="tw-flex tw-flex-col tw-gap-4">
+        <h1>Test Combobox Modal</h1>
+        <ComboBox
+          v-model="selectedOption"
+          :options="options"
+          label="New ComboBox"
+        />
+
+        <OldCombobox
+          v-model="selectedOption"
+          :options="options"
+          label="Old ComboBox"
+        />
+      </div>
+    </Modal>
   </PostIt>
 </template>
 <script setup lang="ts">
@@ -13,6 +89,8 @@ import { ComboBox, ComboBoxOptionType } from "./ComboBox";
 import { computed, ref } from "vue";
 
 import OldCombobox from "@/components/ComboBox.vue";
+import Modal from "@/components/Modal.vue";
+import Button from "@/components/Button.vue";
 
 // const abridgedOptions = computed(() => {
 //   const maxOptions = Math.min(10, options.length);
@@ -20,5 +98,6 @@ import OldCombobox from "@/components/ComboBox.vue";
 // });
 
 const selectedOption = ref<ComboBoxOptionType | null>(null);
+const isModalOpen = ref(false);
 </script>
 <style scoped></style>
