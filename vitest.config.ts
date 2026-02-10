fa7f5de3994aa77e85c5161355e1a32e2b418f03
James Johnson <johnsojr@umn.edu>
2026-02-10 08:46:50 -0600

    silence CI warning about vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.js";
 
+// Allow Vitest to run in CI despite laravel-vite-plugin's env check
+process.env.LARAVEL_BYPASS_ENV_CHECK = "1";
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
