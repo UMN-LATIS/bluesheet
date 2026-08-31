7b91a546d196448bd7ba157d73f38192df1b4082
James Johnson <johnsojr@umn.edu>
2026-08-31 12:43:38 -0500

    resolve seeded admin by umndid and roles by name

--- database/seeders/TestGroupsModelHasRolesTableSeeder.php
+++ database/seeders/TestGroupsModelHasRolesTableSeeder.php
@@ -2,16 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class TestGroupsModelHasRolesTableSeeder extends Seeder {
     public function run(): void {
-        // User 1 is the admin created by TestUsersSeeder.
-        $modelRoles = [
-            ['role_id' => 1, 'model_type' => 'App\\User', 'model_id' => 1],
-            ['role_id' => 2, 'model_type' => 'App\\User', 'model_id' => 1],
-            ['role_id' => 4, 'model_type' => 'App\\User', 'model_id' => 1],
-        ];
+        $admin = User::firstWhere('umndid', 'admin');
+        $roleIds = Role::whereIn('name', ['basic user', 'view user', 'site admin'])->pluck('id');
+
+        $modelRoles = $roleIds->map(fn ($roleId) => [
+            'role_id' => $roleId,
+            'model_type' => 'App\\User',
+            'model_id' => $admin->id,
+        ])->all();
 
         \DB::table('model_has_roles')->upsert($modelRoles, ['role_id', 'model_type', 'model_id']);
     }
