62111eed258f11905b670a280984b654156790a8
James Johnson <johnsojr@umn.edu>
2026-08-31 17:23:04 -0500

    move AppHeader and AppFooter into layouts (#243)

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -1,9 +1,13 @@
 <template>
+  <AppHeader />
   <PostIt class="container-fluid post-it">
     <slot />
   </PostIt>
+  <AppFooter />
 </template>
 <script setup lang="ts">
 import { PostIt } from "@umn-latis/cla-vue-template";
+import { AppFooter } from "@umn-latis/cla-vue-template";
+import AppHeader from "@/components/AppHeader.vue";
 </script>
 <style scoped></style>
