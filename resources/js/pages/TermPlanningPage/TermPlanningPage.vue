07476e61c144df0f8ba0cbd011622bccea43c4ac
James Johnson <johnsojr@umn.edu>
2026-08-31 21:37:10 -0500

    stub schedule grid

--- resources/js/pages/TermPlanningPage/TermPlanningPage.vue
+++ resources/js/pages/TermPlanningPage/TermPlanningPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4">
+  <div class="tw-p-4">
     <header class="tw-mb-8">
       <h1>Term Planning</h1>
       <p class="tw-text-neutral-500">
@@ -7,16 +7,13 @@
       </p>
     </header>
 
-    <dl class="tw-grid tw-grid-cols-[auto_1fr] tw-gap-x-4 tw-gap-y-1">
-      <dt class="tw-font-semibold">Group</dt>
-      <dd>{{ groupId }}</dd>
-      <dt class="tw-font-semibold">Term</dt>
-      <dd>{{ termCode ?? "defaults to the current term" }}</dd>
-    </dl>
+    <ScheduleGrid />
   </div>
 </template>
 
 <script setup lang="ts">
+import ScheduleGrid from "./components/ScheduleGrid.vue";
+
 defineProps<{
   groupId: number;
   termCode: number | null;
