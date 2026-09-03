17a2bdb3b8337d83e38b80d559065b16aaba8f71
Colin McFadden <cmcfadden@gmail.com>
2026-09-03 16:47:55 -0500

    apparently i didn't commit this

--- resources/js/pages/reports/HRReportPage.vue
+++ resources/js/pages/reports/HRReportPage.vue
@@ -57,7 +57,7 @@
           <td>{{ department.zdept_id }}</td>
           <td>
             <router-link :to="{ name: 'group', params: { groupId: department.group.id } }">{{ department.department
-            }}</router-link>
+              }}</router-link>
           </td>
           <td>
             <UserWithLink :memberList="department.hrConsultant"></UserWithLink>
